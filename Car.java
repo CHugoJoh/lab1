bd6a6617c24a735d0da147570f479da6699d3ce8
William Lindgren <william.lindgren19@gmail.com>
2023-11-06 13:30:08 +0100

    Car of Protection

--- Car.java
+++ Car.java
@@ -1,11 +1,11 @@
 import java.awt.*;
 
 public abstract class Car {
-    public int nrDoors; // Number of doors on the car
-    public double enginePower; // Engine power of the car
-    public double currentSpeed; // The current speed of the car
-    public Color color; // Color of the car
-    public String modelName; // The car model name
+    protected int nrDoors; // Number of doors on the car
+    protected double enginePower; // Engine power of the car
+    protected double currentSpeed; // The current speed of the car
+    protected Color color; // Color of the car
+    protected String modelName; // The car model name
 
     public int getNrDoors(){
         return nrDoors;
@@ -34,11 +34,11 @@ public abstract class Car {
 	    currentSpeed = 0;
     }
 
-    public abstract double speedFactor();
+    protected abstract double speedFactor();
 
-    public abstract void incrementSpeed(double amount);
+    protected abstract void incrementSpeed(double amount);
 
-    public abstract void decrementSpeed(double amount);
+    protected abstract void decrementSpeed(double amount);
 
     // TODO fix this method according to lab pm
     public void gas(double amount){

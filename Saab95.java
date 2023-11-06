6c6d78399a2100e99017ca68982990c11513f5ea
Unlucky-e <emil@cederqvist.nu>
2023-11-06 13:32:02 +0100

    protection!

--- Saab95.java
+++ Saab95.java
@@ -2,7 +2,7 @@ import java.awt.*;
 
 public class Saab95 extends Car{
 
-    public boolean turboOn;
+    private boolean turboOn;
     
     public Saab95(){
         nrDoors = 2;
@@ -21,17 +21,17 @@ public class Saab95 extends Car{
 	    turboOn = false;
     }
     @Override
-    public double speedFactor(){
+    protected double speedFactor(){
         double turbo = 1;
         if(turboOn) turbo = 1.3;
         return enginePower * 0.01 * turbo;
     }
     @Override
-    public void incrementSpeed(double amount){
+    protected void incrementSpeed(double amount){
         currentSpeed = getCurrentSpeed() + speedFactor() * amount;
     }
     @Override
-    public void decrementSpeed(double amount){
+    protected void decrementSpeed(double amount){
         currentSpeed = getCurrentSpeed() - speedFactor() * amount;
     }
     

14910004aae476bdaf110fcc86063c6a99db3210
CHugoJoh <hugjoh@chalmers.se>
2023-11-06 13:31:50 +0100

    Fixat volvon igen igen

--- Volvo240.java
+++ Volvo240.java
@@ -2,7 +2,7 @@ import java.awt.*;
 
 public class Volvo240 extends Car{
 
-    public final static double trimFactor = 1.25;
+    private final static double trimFactor = 1.25;
     
     public Volvo240(){
         nrDoors = 4;
@@ -13,17 +13,17 @@ public class Volvo240 extends Car{
     }
     
     @Override
-    public double speedFactor(){
+    protected double speedFactor(){
         return enginePower * 0.01 * trimFactor;
     }
 
     @Override
-    public void incrementSpeed(double amount){
+    protected void incrementSpeed(double amount){
 	    currentSpeed = Math.min(getCurrentSpeed() + speedFactor() * amount,enginePower);
     }
 
     @Override
-    public void decrementSpeed(double amount){
+    protected void decrementSpeed(double amount){
         currentSpeed = Math.max(getCurrentSpeed() - speedFactor() * amount,0);
     }
 

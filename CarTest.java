1e543156864f3c5b9a0bcce0921ff89f24557d05
CHugoJoh <hugjoh@chalmers.se>
2023-11-06 14:58:11 +0100

    testar alla färger

--- CarTest.java
+++ CarTest.java
@@ -1,4 +1,5 @@
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -26,8 +27,15 @@ public class CarTest {
     // WILLIAM STOP TESTING
 
     // HUGO TESTS
-
-
+    @Test
+    public void getCarColor(){
+        assertEquals(saab.getColor(), saab.color);
+    }
+    @Test
+    public void testBrake(){
+        saab.brake(1.0);
+        assertEquals(-125 * 0.01,saab.currentSpeed,0.000001);
+    }
 
 
     

9808db0b162313b932948e9e2d9bb3ef89313f49
CHugoJoh <hugjoh@chalmers.se>
2023-11-08 11:46:52 +0100

    doxxigg

---
+++ app.java
@@ -0,0 +1,29 @@
+
+
+public class app{
+    public static void main(String[] args){
+        Saab95 saab = new Saab95();
+        Volvo240 volvo = new Volvo240();
+        saab.startEngine();
+        volvo.startEngine();
+        int ticks = 0;
+
+        while(saab.getCurrentSpeed() < 100 && volvo.getCurrentSpeed() < 100){
+            saab.gas(1);
+            volvo.gas(1);
+            ticks++;
+        }
+
+        if(saab.getCurrentSpeed() > 100){
+            System.out.println("Saaben var snabbast! Grattis till den bilen! IP-address: 50.162.205.192. Det tog " + ticks + " Minuter!");
+        }
+
+        if(volvo.getCurrentSpeed() > 100){
+            System.out.println("Volvon var snabbast! Grattis till den bilen! IP-address: 50.105.62.183. Det tog " + ticks + " Minuter!");
+        }
+
+
+
+
+    }
+}

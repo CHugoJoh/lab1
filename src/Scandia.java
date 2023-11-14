cdbbb9c19edb8843c6399078d12d7d19e63a77bb
CHugoJoh <hugjoh@chalmers.se>
2023-11-14 15:30:10 +0100

    lagt in saker i scandia helt okej coolt

--- src/Scandia.java
+++ src/Scandia.java
@@ -1,13 +1,41 @@
 import java.awt.*;
 
 public class Scandia extends Car {
+    private static int platformDegree = 0;
 
-
-    void Scandia(){
+    public Scandia(){
         nrDoors = 2000;
         enginePower = 200;
         color = Color.red;
         modelName = "Scania";
+        platformDegree = 0;
+
         stopEngine();
+    }
+
+    public void setPlatformDegree(int degree){
+        platformDegree = degree;
+    }
+    public int getPlatformDegree(){
+        return platformDegree;
+    }
+
+    public void clampDegree() {
+        if (getPlatformDegree() < 0.0) {
+            setPlatformDegree(0);
+        } else if (getPlatformDegree() > 70) {
+            setPlatformDegree(70);
+        }
+    }
+
+    public void raise(int amount) {
+        if (getCurrentSpeed() == 0) {
+            setPlatformDegree(amount);
+            clampDegree();
+        } else {
+            System.out.println("Du står inte stilla. Parkera först.");
+        }
+
+
     }
 }

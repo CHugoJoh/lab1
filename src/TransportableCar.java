784dde78d296ce09497bd46e182f96ea1264c152
CHugoJoh <hugjoh@chalmers.se>
2023-11-15 11:40:33 +0100

    i am the beast i worship

---
+++ src/TransportableCar.java
@@ -0,0 +1,10 @@
+public abstract class TransportableCar extends Car implements Transportable{
+    public boolean isCloseEnough(double x, double y){
+        double dX = x - getX();
+        double dY = y - getY();
+        double hypotenuse = Math.hypot(dX,dY);
+
+        return hypotenuse < 5;
+    }
+
+}

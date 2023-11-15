1b390f09de1fed21c886af97ef7e1c96ad9f6e94
William Lindgren <william.lindgren19@gmail.com>
2023-11-15 11:03:10 +0100

    revert

--- src/HasRamp.java
+++ src/HasRamp.java
@@ -1,8 +1,5 @@
 public interface HasRamp {
     public void raiseRamp();
     public void lowerRamp();
-    public double getRampDegree();         
-    default public boolean isRampDown() {
-        return !(getRampDegree() > 0.0);
-    }                                                         
+    public double getRampDegree();                                                       
 }

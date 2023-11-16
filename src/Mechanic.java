c814eb50ecacb9593e4b91ba352f5d511da922c8
Unlucky-e <emil@cederqvist.nu>
2023-11-16 10:11:20 +0100

    Update Mechanic.java
    trial 1

--- src/Mechanic.java
+++ src/Mechanic.java
@@ -1,3 +1,9 @@
-public class Mechanic{
-    
+import java.util.*;
+
+public class Mechanic<T extends Car> {
+    public void load(T Car){
+        ArrayList<T> cars = new ArrayList<>();
+        cars.add Car;
+    }
+
 }
